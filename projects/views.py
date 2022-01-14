80154a6c825c19a97cb9c111a42b5ddc6f45e7e1
lejames <okejames55@gmail.com>
2022-01-14 14:47:40 +0300

    Projects, Reviews, Tags models

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
+def projects(request):
+    page = "projects"
+    number = 9
+    context = {'page': page, 'number': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context )
+
+def project(request,pk):
+    projectObj = None
+    for project in projectsList:
+        if project["id"] == pk:
+            projectObj = project
+    return render(request, 'projects/single-project.html', {'project': projectObj})
+

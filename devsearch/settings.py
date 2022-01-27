ab4386901992123de68c153034904881e953954c
lejames <okejames55@gmail.com>
2022-01-27 16:29:17 +0300

    users app

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects.apps.ProjectsConfig'
+
+    'projects.apps.ProjectsConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [

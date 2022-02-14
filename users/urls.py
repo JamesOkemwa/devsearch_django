08a5c05ca9ed85a4f363600ef8602d6143b55bc1
lejames <okejames55@gmail.com>
2022-02-14 17:55:54 +0300

    User registration

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.loginUser, name="login"),
     path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
 
 
     path('', views.profiles, name="profiles"),

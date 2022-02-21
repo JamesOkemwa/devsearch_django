0dbc6744ea92292b708b028526d4ed8f5cfb91f6
lejames <okejames55@gmail.com>
2022-02-21 10:39:36 +0300

    Projects and profiles pagination

--- projects/utils.py
+++ projects/utils.py
@@ -1,5 +1,34 @@
 from django.db.models import Q 
 from .models import Project, Tag 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage 
+
+def paginateProjects(request, projects, results):
+    page = request.GET.get('page')
+    paginator = Paginator(projects, results)
+
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        # page is out of range. Return the last page
+        page = paginator.num_pages
+        projects = paginator.page(page)
+
+    leftIndex = (int(page) - 4)
+
+    if leftIndex < 1:
+        leftIndex = 1
+
+    rightIndex = (int(page) + 5)
+
+    if rightIndex > paginator.num_pages:
+        rightIndex = paginator.num_pages + 1
+
+    custom_range = range(leftIndex, rightIndex)
+
+    return custom_range, projects
 
 def searchProjects(request):
     search_query = ''

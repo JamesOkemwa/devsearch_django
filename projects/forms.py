ac3045553f11624beb3702130d5703766cca07ea
lejames <okejames55@gmail.com>
2022-02-22 19:19:23 +0300

    project reviews

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Project
+from .models import Project, Review
 
 class ProjectForm(ModelForm):
     class Meta:
@@ -19,3 +19,19 @@ class ProjectForm(ModelForm):
         # self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add title'})
 
         # self.fields['description'].widget.attrs.update({'class': 'input'})
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review 
+        fields = ['value', 'body']
+        labels = {
+            'value': 'Place your vote',
+            'body': 'Add a comment with your vote'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ def loginUser(request):
         return redirect('profiles')
 
     if request.method == "POST":
-        username = request.POST['username']
+        username = request.POST['username'].lower()
         password = request.POST['password']
 
         try:
@@ -30,7 +30,7 @@ def loginUser(request):
 
         if user is not None:
             login(request, user) # creates a session for the user in the database
-            return redirect('profiles')
+            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
         else:
             messages.error(request, 'Username or password is incorrect')
 
